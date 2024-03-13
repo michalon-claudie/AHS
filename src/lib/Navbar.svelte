f3fda260fff6c57d061b9dc4fb4a389850aeb783
Claudie.Michalon <michalon.claudie@outlook.fr>
2024-03-13 09:34:12 +0100

    add tailwind and create navbar

---
+++ src/lib/Navbar.svelte
@@ -0,0 +1,10 @@
+<nav>
+    <a href="/">
+        <img src="" alt=""/>
+    </a>
+    <div>
+        <a href="/">Home</a>
+        <a href="/about">About</a>
+        <a href="last_news">What's New</a>
+    </div>
+</nav>
