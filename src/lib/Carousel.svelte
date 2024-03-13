28a330a5eef7bfb9067b07071e1bec9a3087e0d5
Claudie.Michalon <michalon.claudie@outlook.fr>
2024-03-13 16:36:42 +0100

    add carousel and add style

---
+++ src/lib/Carousel.svelte
@@ -0,0 +1,33 @@
+<script>
+    import ahs1 from '../images/ahs1.webp'
+    import ahs2 from '../images/ahs2.webp'
+    import ahs3 from '../images/ahs3.webp'
+    let CurrentIndex = 0;
+    const images=[ahs1,ahs2,ahs3]
+
+    function nextImages(){
+        CurrentIndex=(CurrentIndex+1)%images.length;
+    }
+
+    function previousImages(){
+        CurrentIndex=(CurrentIndex-1 + images.length)%images.length;
+    }
+</script>
+
+<section class="relative">
+    {#each images as image, i}
+        <img
+        src={image}
+        alt={`Image ${i + 1}`}
+        class="{i === CurrentIndex ? 'block' : 'hidden'} 
+        absolute w-full h-72 object-cover transition-opacity duration-500"
+    />
+    {/each}
+
+    <button class="absolute top-1/2 transform -translate-y-1/2 left-4" on:click={previousImages}>
+        Précédent
+    </button>
+    <button class="absolute top-1/2 transform -translate-y-1/2 right-4" on:click={nextImages}>
+        Suivant
+    </button>
+</section>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,12 @@
 <script>
     import Navbar from "$lib/Navbar.svelte"
+    import Carousel from "$lib/Carousel.svelte"
     import Footer from "$lib/Footer.svelte"
 </script>
 
 <div class="bg-black text-white">
     <Navbar/>
     <h1 class="text-3xl">Hello world</h1>
+    <Carousel/>
     <Footer/>
 </div>
