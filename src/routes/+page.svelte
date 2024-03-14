e3c0dd5953a9cbfffe9a891c274115174c29a936
Claudie.Michalon <michalon.claudie@outlook.fr>
2024-03-14 19:03:52 +0100

    fix bugs and delete type

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,8 @@
 <script>
-    import Navbar from "$lib/Navbar.svelte"
-    import Carousel from "$lib/Carousel.svelte"
-    import Footer from "$lib/Footer.svelte"
+    import Navbar from "../lib/Navbar.svelte"
+    import Carousel from "../lib/Carousel.svelte"
+    import Footer from "../lib/Footer.svelte"
+
 </script>
 
 <div class="bg-black text-white">
